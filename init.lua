86176457f34b7cab244c93e33554d86e587cf2fc
Jonathan Cooper <DrowsySaturn@gmail.com>
2023-11-13 01:51:14 -0500

    Add snippets

--- init.lua
+++ init.lua
@@ -1,2 +1,4 @@
+vim.o.guifont = "MonoLisa:h12"
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+require("config.snippets")
---
+++ lua/config/snippets.lua
@@ -0,0 +1,6 @@
+ls = require("luasnip")
+ls.add_snippets("autohotkey", {
+  ls.snippet("ahk2", {
+    ls.text_node("#Requires AutoHotkey v2.0"),
+  }),
+})
