08e8997c9860538869c86b5a93097f16c7b39aa0
Jonathan Cooper <DrowsySaturn@gmail.com>
2024-01-11 10:23:31 -0500

    Remove annoying html tag completion

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,10 +1,22 @@
 return {
+  -- Disable annoying auto completion of ( and {
   {
     "echasnovski/mini.pairs",
     enabled = false,
   },
+  -- Disable annoying auto movement to insides and end of function args when pressing tab
   {
     "echasnovski/mini.surround",
+    enabled = false,
+  },
+  -- Disable annoying html tag completion in html and jsx
+  {
+    "windwp/nvim-ts-autotag",
+    enabled = false,
+  },
+  -- Disable annoying search change
+  {
+    "folke/flash.nvim",
     enabled = false,
   },
 }
