03447d2a490d1ccbf5fef8df39476adea6b3378e
Jonathan Cooper <DrowsySaturn@gmail.com>
2024-11-04 20:08:01 -0800

    Add elixir support

--- init.lua
+++ init.lua
@@ -125,6 +125,13 @@ require("lazy").setup(
 
     {
       "nvim-treesitter/nvim-treesitter",
+      config = function()
+        require("nvim-treesitter.configs").setup({
+          ensure_installed = { "elixir", "eex", "heex" },
+          highlight = { enable = true },
+          indent = { enable = true },
+        })
+      end,
     },
 
     {
