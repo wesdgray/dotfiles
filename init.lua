07a067fae2285c37ad53a89a9539a9a64d20e0a5
Jonathan Cooper <drowsysaturn@gmail.com>
2024-11-24 22:45:28 -0500

    Auto setup deno lsp from local
    If the deno executable is detected, then setup the deno language server.

--- init.lua
+++ init.lua
@@ -289,6 +289,9 @@ require("lazy").setup(
         if vim.fn.executable("rust-analyzer") then
           require("lspconfig")["rust_analyzer"].setup({})
         end
+        if vim.fn.executable("deno") then
+          require("lspconfig")["denols"].setup({})
+        end
       end,
       keys = {
         {
